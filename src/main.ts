7e2c6917efe0d4f149eb7a6c73c6d8a38d1b242e
Steve <sfailla1983@gmail.com>
2026-01-19 16:28:57 -0500

    add comment for Hot Module Replacement (HMR) in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const app = await Game.create({ canvas })
 
 app.start()
 
+// Hot Module Replacement (HMR) - Remove this snippet for production
 if (import.meta.hot) {
     import.meta.hot.dispose(() => app.destroy())
 }
